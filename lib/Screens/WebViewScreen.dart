7ce62974ec07ff87f94e1ece1dc06f1b9971ae49
irfanbacker <irfuvk@gmail.com>
2021-02-27 17:16:28 +0530

    Added webview support

--- lib/Screens/WebViewScreen.dart
+++ lib/Screens/WebViewScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class WebViewScreen extends StatelessWidget {
   WebViewScreen({Key key, this.url}) : super(key: key);
@@ -7,12 +8,17 @@ class WebViewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WebviewScaffold(
       appBar: AppBar(
         title: Text('webview'),
         backgroundColor: Colors.white,
+        elevation: 10.0,
       ),
-      body: Container(),
+      initialChild: Center(
+        child: CircularProgressIndicator(),
+      ),
+      withZoom: true,
+      url: url,
     );
   }
 }
