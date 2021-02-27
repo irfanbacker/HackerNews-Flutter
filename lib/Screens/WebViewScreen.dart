1ef7b321f414adb50772648342b24a0f3c938d3f
irfanbacker <irfuvk@gmail.com>
2021-02-27 15:11:35 +0530

    Basic structure for app

---
+++ lib/Screens/WebViewScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WebViewScreen extends StatelessWidget {
+  WebViewScreen({Key key, this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Widgets/NewsCard.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewsCard extends StatelessWidget {
+  NewsCard({Key key, this.newsItem}) : super(key: key);
+
+  final newsItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
