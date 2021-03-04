953309e7603f84f6079efadf071b99e33896ebff
irfanbacker <irfuvk@gmail.com>
2021-03-04 16:51:26 +0530

    Change webview plugin to official flutter plugin

--- lib/Screens/WebViewScreen.dart
+++ lib/Screens/WebViewScreen.dart
@@ -1,24 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'dart:io';
 
-class WebViewScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
   WebViewScreen({Key key, this.url}) : super(key: key);
 
   final String url;
 
   @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('webview'),
         backgroundColor: Colors.white,
         elevation: 10.0,
       ),
-      initialChild: Center(
-        child: CircularProgressIndicator(),
+      body: WebView(
+        initialUrl: widget.url,
       ),
-      withZoom: true,
-      url: url,
     );
   }
 }
